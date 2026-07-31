6a3803741114388c65bcfcba5ba036222fe80a96
Muhammad Taha <tahabutt627@gmail.com>
2026-07-31 23:17:43 +0500

    Custom Hook For Authentication

--- car-rental-frontend/src/hooks/useAuth.ts
+++ car-rental-frontend/src/hooks/useAuth.ts
@@ -1,1 +1,17 @@
 // makes using AuthContext.tsx easier
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuth must be used inside an AuthProvider."
+    );
+  }
+
+  return context;
+};
+
+export default useAuth;
