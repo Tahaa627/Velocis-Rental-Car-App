4f8a022c23527ebbba05fb1229c0475a148dfe44
Muhammad Taha <tahabutt627@gmail.com>
2026-07-31 23:14:10 +0500

    Created AuthContext Provider

--- car-rental-frontend/src/context/AuthContext.tsx
+++ car-rental-frontend/src/context/AuthContext.tsx
@@ -1,21 +1,39 @@
-// act as a backpack 
-// Stores User Token, login and logout
 import {createContext,useState,type ReactNode,} from "react";
 
 interface User {
-
-    id: string;
-    name: string;
-    email: string;
+  id: string;
+  name: string;
+  email: string;
 }
 
 interface AuthContextType {
-
-    user: User | null;
-    login: (user: User) => void;
-    logout: () => void;
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(
-    undefined
+  undefined
 );
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (userData: User) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{user,login,logout,}} >
+      {children}
+    </AuthContext.Provider>
+  );
+};
